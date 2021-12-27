5a44f355c815bf9c7b8b47fc55cdc97112d33574
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2021-12-27 21:17:37 +0530

    return distinct status on not signed-in vs unsubscribed in CDN auth

--- src/main/java/com/trynoice/api/sound/SoundService.java
+++ src/main/java/com/trynoice/api/sound/SoundService.java
@@ -1,12 +1,14 @@
 package com.trynoice.api.sound;
 
 import com.trynoice.api.identity.entities.AuthUser;
-import com.trynoice.api.sound.exceptions.SegmentRequestAuthorizationException;
+import com.trynoice.api.sound.exceptions.SegmentAccessDeniedException;
 import com.trynoice.api.subscription.SubscriptionService;
 import lombok.NonNull;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolationException;
 
 /**
  * {@link SoundService} implements operations related to sound.
@@ -26,22 +28,20 @@ class SoundService {
     /**
      * Authorizes a request for the given {@code segmentId} of the given {@code soundId}. If the
      * requested segment is premium, {@code principal} must be non-null, and must have an active
-     * subscription. Throws {@link SegmentRequestAuthorizationException} if {@code principal} is not
+     * subscription. Throws {@link SegmentAccessDeniedException} if {@code principal} is not
      * authorized to access the requested segment.
      *
      * @param principal an {@code nullable} {@link AuthUser} that made this request.
      * @param soundId   the id of the sound being requested.
      * @param segmentId the id of the segment being requested.
-     * @throws SegmentRequestAuthorizationException if the request should not be authorized.
+     * @throws SegmentAccessDeniedException if {@code principal} requests a premium segment but
+     *                                      isn't signed-in ({@code null}) or doesn't have an active
+     *                                      subscription.
      */
-    void authorizeSegmentRequest(
-        AuthUser principal,
-        @NonNull String soundId,
-        @NonNull String segmentId
-    ) throws SegmentRequestAuthorizationException {
+    void authorizeSegmentRequest(AuthUser principal, @NonNull String soundId, @NonNull String segmentId) throws SegmentAccessDeniedException {
         val premiumSegmentMappings = libraryManifestRepository.getPremiumSegmentMappings();
         if (!premiumSegmentMappings.containsKey(soundId)) {
-            throw new SegmentRequestAuthorizationException(String.format("sound with id '%s' doesn't exist", soundId));
+            throw new ConstraintViolationException(String.format("sound with id '%s' doesn't exist", soundId), null);
         }
 
         // reversed search using startsWith and endsWith to cover bridge segments since they are not
@@ -55,11 +55,11 @@ class SoundService {
         }
 
         if (principal == null) {
-            throw new SegmentRequestAuthorizationException("user is not signed-in");
+            throw new SegmentAccessDeniedException("user is not signed-in");
         }
 
         if (!subscriptionService.isUserSubscribed(principal)) {
-            throw new SegmentRequestAuthorizationException("user doesn't have an active subscription");
+            throw new SegmentAccessDeniedException("user doesn't have an active subscription");
         }
     }
 }
