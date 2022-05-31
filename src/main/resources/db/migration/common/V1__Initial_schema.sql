decde7b83e30763f459743a6b9a1da9b5cfe0608
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-05-31 15:00:49 +0530

    add separate subscription plans production and test mode

--- src/main/resources/db/migration/common/V1__Initial_schema.sql
+++ src/main/resources/db/migration/common/V1__Initial_schema.sql
@@ -51,14 +51,14 @@ CREATE TABLE subscription_plan (
 
 INSERT INTO subscription_plan (created_at, version, provider, provider_plan_id, billing_period_months, trial_period_days, price_in_indian_paise)
   VALUES
-    (now(), 0, 'GOOGLE_PLAY', 'monthly', 1, 14, 22500),
-    (now(), 0, 'GOOGLE_PLAY', 'quarterly', 3, 14, 60000),
-    (now(), 0, 'GOOGLE_PLAY', 'bi_yearly', 6, 14, 105000),
-    (now(), 0, 'GOOGLE_PLAY', 'yearly', 12, 14, 180000),
-    (now(), 0, 'STRIPE', 'price_1Ko0DFSEeVq01jORuGMXnINT', 1, 14, 22500),
-    (now(), 0, 'STRIPE', 'price_1Ko0DFSEeVq01jORt7saWcXM', 3, 14, 60000),
-    (now(), 0, 'STRIPE', 'price_1Ko0DFSEeVq01jOR9aCG7EM8', 6, 14, 105000),
-    (now(), 0, 'STRIPE', 'price_1Ko0DFSEeVq01jORgHlVkTEN', 12, 14, 180000);
+    (now(), 0, 'GOOGLE_PLAY', 'premium', 1, 14, 22500),
+    (now(), 0, 'GOOGLE_PLAY', 'premium', 3, 14, 60000),
+    (now(), 0, 'GOOGLE_PLAY', 'premium', 6, 14, 105000),
+    (now(), 0, 'GOOGLE_PLAY', 'premium', 12, 14, 180000),
+    (now(), 0, 'STRIPE', 'price_1L5R9JSEeVq01jORQPQdUC5J', 1, 14, 22500),
+    (now(), 0, 'STRIPE', 'price_1L5R9JSEeVq01jORRBhkQFoC', 3, 14, 60000),
+    (now(), 0, 'STRIPE', 'price_1L5R9JSEeVq01jORI9QGRC3w', 6, 14, 105000),
+    (now(), 0, 'STRIPE', 'price_1L5R9JSEeVq01jORWCxWXcDO', 12, 14, 180000);
 
 CREATE TABLE customer (
   user_id bigint NOT NULL PRIMARY KEY,
